efa9d2864caf39faa4afb3eaaf7e31b0f5f054cd
root <root@localhost.creatusello.com>
2020-01-27 12:25:01 +0530

    add send email API

--- api/controllers/SendEmailController.php
+++ api/controllers/SendEmailController.php
@@ -4,6 +4,8 @@ namespace api\controllers;
 
 /* USE COMMON MODELS */
 use common\components\Common;
+use common\models\EmailFormat;
+use common\models\SentNotes;
 use common\models\Users;
 use kartik\mpdf\Pdf;
 use Yii;
@@ -27,19 +29,19 @@ class SendEmailController extends \yii\base\Controller
             Common::encodeResponseJSON($amResponse);
         }
         $requestParam = $amData['request_param'];
-        //  p($requestParam);
-        $notes = $requestParam['notes'];
+        $notes = json_decode(json_encode($requestParam['notes']), true);
+        // p($notes[1]);
 
-        /*  $amRequiredParamsNotes = array('id', 'color_code', 'title', 'font_name', 'font_size', 'patient_id', 'description');
+        $amRequiredParamsNotes = array('note_id', 'color_code', 'title', 'font_name', 'font_size', 'patient_id', 'description');
 
         foreach ($notes as $key => $note) {
-        $amParamsResultNotes = Common::checkRequestParameterKey($note, $amRequiredParamsNotes);
+            $amParamsResultNotes = Common::checkRequestParameterKey((array) json_decode($note), $amRequiredParamsNotes);
 
-        if (!empty($amParamsResultNotes['error'])) {
-        $amResponse = Common::errorResponse($amParamsResultNotes['error']);
-        Common::encodeResponseJSON($amResponse);
+            if (!empty($amParamsResultNotes['error'])) {
+                $amResponse = Common::errorResponse($amParamsResultNotes['error']);
+                Common::encodeResponseJSON($amResponse);
+            }
         }
-        }*/
         // Check User Status
         Common::matchUserStatus($requestParam['user_id']);
         //VERIFY AUTH TOKEN
@@ -52,16 +54,51 @@ class SendEmailController extends \yii\base\Controller
             foreach ($notes as $key => $note) {
                 # code...
                 $noteArr = json_decode($note);
-
-                $html = '<html xmlns="http://www.w3.org/1999/xhtml">
-                  <head style="background-color:"' . $noteArr->color_code . '>
-                      <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-                      <h1 style="margin:0,padding:8">' . $noteArr->title . '</h1>
-                  </head>
-                  <body style="margin:0,padding:0">
-                  <p>' . $noteArr->description . '</p>
-                  </body>
-                  </html>';
+                $html = '<!DOCTYPE html>
+                <html>
+                    <head>
+                        <title>Enroot</title>
+                        <meta charset="UTF-8">
+                        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                        <link href="https://fonts.googleapis.com/css?family=Poppins:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i&display=swap" rel="stylesheet">
+                        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+                        <style>
+                            html{height: 100%;background: #fff;}
+                            header{background: #5f5f9c;padding: 30px 0px;border-radius: 50px 50px 0px 0px;border: 0.5px solid #ddd;}
+                            header h1{color: #fff;font-family: ' . !empty($noteArr->font_name) ? $noteArr->font_name : "Poppins" . ', sans-serif;font-weight: 400;letter-spacing: 0px;text-transform: capitalize;font-size: ' . !empty($noteArr->font_size) ? $noteArr->font_size : "40" . ';line-height: ' . !empty($noteArr->font_size) ? $noteArr->font_size : "40px" . ';text-align: center;}
+                             section{background: #fff;}
+                            section p{color: #333;font-family: "Poppins", sans-serif;font-weight: 400;letter-spacing: 0px;text-transform: capitalize;font-size: 14px;line-height: 20px;margin: 10px 0px;}
+                        </style>
+                    </head>
+                <body>
+​
+                    <header>
+                        <div class="container-fluid">
+                        <div class="row">
+                        <div class="col-md-12 p-0">
+                        <h1>Adverse Event...</h1>
+                        </div>
+                        </div>
+                        </div>
+                        </header>
+                        <section>
+                            <div class="container-fluid">
+                            <div class="row">
+                            <div class="col-md-12">
+                            <p> This is the operating system that powers many Apple-based devices ranging from
+                            iPhone, iPad, etc. offering multiple innovative features enhancing user experience. The
+                            benefits it provides are endless, given the integration of dynamic Apple features on their
+                            devices making it the second most popular OS.</p>
+                                <p>This is the operating system that powers many Apple-based devices ranging from
+                            iPhone, iPad, etc. offering multiple innovative features enhancing user experience. The
+                            benefits it provides are endless, given the integration of dynamic Apple features on their
+                            devices making it the second most popular OS.</p>
+                            </div>
+                            </div>
+                            </div>
+                        </section>
+                </body>
+            </html>';
 //            $content = $this->renderPartial('_reportView');
 
                 // setup kartik\mpdf\Pdf component
@@ -92,21 +129,37 @@ class SendEmailController extends \yii\base\Controller
                 $pdf->content = $html;
                 $file_name = "note_" . rand(7, 100) . "_" . time() . ".pdf";
                 $pdf->filename = "../../uploads/pdf_files/" . $file_name;
-                return $pdf->render();
-                /*       $emailformatemodel = EmailFormat::findOne(["title" => 'user_registration', "status" => '1']);
-            if ($emailformatemodel) {
+                echo $pdf->render();
+                $emailformatemodel = EmailFormat::findOne(["title" => 'note_email', "status" => '1']);
+                if ($emailformatemodel) {
 
-            //create template file
-            $AreplaceString = array('{password}' => $requestParam['password'], '{username}' => $model->user_name, '{email}' => $model->email, '{email_verify_link}' => $email_verify_link);
+                    //create template file
+                    /*   $AreplaceString = array('{password}' => $requestParam['password'], '{username}' => $model->user_name, '{email}' => $model->email, '{email_verify_link}' => $email_verify_link);
 
-            $body = Common::MailTemplate($AreplaceString, $emailformatemodel->body);
-            $ssSubject = $emailformatemodel->subject;
-            //send email for new generated password
-            $ssResponse = Common::sendMail($model->email, Yii::$app->params['adminEmail'], $ssSubject, $body);
+                    $body = Common::MailTemplate($AreplaceString, $emailformatemodel->body);*/
+                    $body = $emailformatemodel->body;
+                    $ssSubject = $emailformatemodel->subject;
+                    //send email for new generated password
+                    $attach = !empty($file_name) && file_exists(Yii::getAlias('@root') . '/' . "uploads/pdf_files/" . $file_name) ? Yii::$app->params['root_url'] . '/' . "uploads/pdf_files/" . $file_name : "";
+                    $ssResponse = Common::sendMailToUserWithAttachment($noteArr->patient_id, $fromEmail, $ssSubject, $body, $attach);
+                    if ($ssResponse) {
+                        $sentNotesModel = new SentNotes();
+                        $sentNotesModel->note_id = $noteArr->note_id;
+                        $sentNotesModel->color_code = $noteArr->color_code;
+                        $sentNotesModel->title = $noteArr->title;
+                        $sentNotesModel->description = $noteArr->description;
+                        $sentNotesModel->from_user_id = $requestParam['user_id'];
+                        $sentNotesModel->to_patient_id = !empty($requestParam['to_patient_id']) ? $requestParam['to_patient_id'] : "";
+                        $sentNotesModel->to_email_id = $noteArr->patient_id;
+                        $sentNotesModel->font_size = $noteArr->font_size;
+                        $sentNotesModel->font_name = $noteArr->font_name;
+                        $sentNotesModel->pdf_filename = $file_name;
+                        $sentNotesModel->save(false);
+                    }
 
-            }*/
+                }
             }
-            $ssMessage = 'Successfully added PDF';
+            $ssMessage = 'Your Email is successfully sent.';
             $amResponse = Common::errorResponse($ssMessage);
         } else {
             $ssMessage = 'Invalid user_id';
