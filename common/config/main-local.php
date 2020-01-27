ad7f0d30efbc4f2a7c6cdca1f20052df61e5c40a
root <root@localhost.creatusello.com>
2020-01-27 15:40:31 +0530

    mail

--- common/config/main-local.php
+++ common/config/main-local.php
@@ -17,14 +17,14 @@ return [
             'useFileTransport' => false,
 //'useFileTransport' => false,//to send mails to real email addresses else will get stored in your mail/runtime folder
             //comment the following array to send mail using php's mail function
-            /*'transport' => [
-        'class' => 'Swift_SmtpTransport',
-        'host' => 'smtp.gmail.com',
-        'username' => 'askb4pet@gmail.com',
-        'password' => 'Temppassword!',
-        'port' => '587',
-        'encryption' => 'tls',
-        ],*/
+            'transport' => [
+                'class' => 'Swift_SmtpTransport',
+                'host' => 'smtp.gmail.com',
+                'username' => 'askb4pet@gmail.com',
+                'password' => 'Temppassword!',
+                'port' => '587',
+                'encryption' => 'tls',
+            ],
         ],
     ],
 ];
