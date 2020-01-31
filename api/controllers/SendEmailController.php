2ff9f40325fdfdb195887750aa529a2f11800827
root <root@localhost.creatusello.com>
2020-01-31 16:59:49 +0530

    add api todo list

--- api/controllers/SendEmailController.php
+++ api/controllers/SendEmailController.php
@@ -275,7 +275,7 @@ class SendEmailController extends \yii\base\Controller
         $amData = Common::checkRequestType();
         $amResponse = array();
         $ssMessage = '';
-        $amRequiredParams = array('user_id', 'protocol', 'investigator', 'date', 'list', 'to_patient_email');
+        $amRequiredParams = array('user_id', 'protocol', 'investigator', 'date', 'to_patient_email');
         $amParamsResult = Common::checkRequestParameterKey($amData['request_param'], $amRequiredParams);
         // If any getting error in request paramter then set error message.
         if (!empty($amParamsResult['error'])) {
@@ -283,25 +283,6 @@ class SendEmailController extends \yii\base\Controller
             Common::encodeResponseJSON($amResponse);
         }
         $requestParam = $amData['request_param'];
-        $list = json_decode($requestParam['list']);
-        $list_arr = "<ul>";
-        foreach ($list as $key => $single_list) {
-            $checked = ($single_list->is_cheked == 1) ? "checked" : "unchecked";
-            $list_arr .= '<li><input type="checkbox" id="' . $key . '" checked="' . $checked . '"><label for="' . $key . '"><span>' . $single_list->text . '</span></label> </li>';
-        }
-        $list_arr = $list_arr . "</ul>";
-        // $list = $requestParam['list'];
-
-        /*    $amRequiredParamsList = array('text', 'is_checked');
-
-        foreach ($list as $key => $single_list) {
-        $amParamsResultList = Common::checkRequestParameterKey((array) $single_list, $amRequiredParamsList);
-
-        if (!empty($amParamsResultList['error'])) {
-        $amResponse = Common::errorResponse($amParamsResultList['error']);
-        Common::encodeResponseJSON($amResponse);
-        }
-        }*/
         // Check User Status
         Common::matchUserStatus($requestParam['user_id']);
         //VERIFY AUTH TOKEN
@@ -312,11 +293,32 @@ class SendEmailController extends \yii\base\Controller
             Common::encodeResponseJSON($amResponse);
         }
         Common::checkAuthentication($authToken, $requestParam['user_id']);
+        $amRequiredParamsList = array('text', 'is_cheked');
 
-        $userModel = Users::findOne(['id' => $requestParam['user_id']]);
-        if (!empty($userModel)) {
-            $fromEmail = $userModel->email;
-            $html = '<!DOCTYPE html>
+        if (!empty($requestParam['list'])) {
+            $list = $requestParam['list'];
+            foreach ($list as $key => $single_list) {
+                $amParamsResultList = Common::checkRequestParameterKey($single_list, $amRequiredParamsList);
+
+                if (!empty($amParamsResultList['error'])) {
+                    $amResponse = Common::errorResponse($amParamsResultList['error']);
+                    Common::encodeResponseJSON($amResponse);
+                }
+            }
+            $list_arr = '<div class="row"><div class="col-md-12 SectionList"><nav><ul>';
+
+            foreach ($list as $key => $single_list) {
+                $checked = ($single_list['is_cheked'] == 1) ? "checked" : "unchecked";
+                $list_arr .= '<li><input type="checkbox" id="list" checked="' . $checked . '"><label for="list"><span>' . $single_list['text'] . '</span></label> </li>';
+            }
+            $list_arr = $list_arr . "</ul></nav></div></div>";
+            // $list = $requestParam['list'];
+
+            $userModel = Users::findOne(['id' => $requestParam['user_id']]);
+            $logo = Yii::$app->params['root_url'] . "/uploads/images/logo-orange.png";
+            if (!empty($userModel)) {
+                $fromEmail = $userModel->email;
+                $html = '<!DOCTYPE html>
                     <html>
                     <head>
                         <meta charset="UTF-8">
@@ -336,32 +338,25 @@ class SendEmailController extends \yii\base\Controller
                         <section>
                             <div class="container-fluid">
                                 <div class="row">
-                                    <div class="col-md-4">
+                                    <div class="col-md-4 col-sm-4">
                                         <div class="Box">
                                             <p class="p_id">' . $requestParam["protocol"] . '</p>
                                             <p>PROTOCOL</p>
                                         </div>
                                     </div>
-                                    <div class="col-md-4">
+                                    <div class="col-md-4 col-sm-4">
                                         <div class="Box">
                                             <p class="p_id">' . $requestParam["investigator"] . '</p>
                                             <p>INVESTIGATOR</p>
                                         </div>
                                     </div>
-                                    <div class="col-md-4">
+                                    <div class="col-md-4 col-sm-4">
                                         <div class="Box">
                                             <p class="p_id">' . $requestParam["date"] . '</p>
                                             <p>DATE</p>
                                         </div>
                                     </div>
-                                </div>
-                                <div class="row">
-                                    <div class="col-md-12 SectionList">
-                                        <nav>
-                                            ' . $list_arr . '
-                                        </nav>
-                                    </div>
-                                </div>
+                                </div>' . $list_arr . '
                             </div>
                         </section>
                         <footer>
@@ -372,7 +367,7 @@ class SendEmailController extends \yii\base\Controller
                                             <p class="BottomText">Resources and Tools for Clinical Research Professionals</p>
                                         </div>
                                         <div class="Logo">
-                                            <img src="' . Yii::$app->params['root_url'] . '/images/logo-orange.png" alt="" class="img-fluid">
+                                            <img src="' . $logo . '" alt="" class="img-fluid">
                                         </div>
                                     </div>
                                 </div>
@@ -382,64 +377,69 @@ class SendEmailController extends \yii\base\Controller
                     </html>';
 //            $content = $this->renderPartial('_reportView');
 
-            // setup kartik\mpdf\Pdf component
-            $pdf = new Pdf([
-                // set to use core fonts only
-                'mode' => Pdf::MODE_CORE,
-                // A4 paper format
-                'format' => Pdf::FORMAT_A4,
-                // portrait orientation
-                'orientation' => Pdf::ORIENT_PORTRAIT,
-                // stream to browser inline
-                'destination' => Pdf::DEST_FILE,
-                // your html content input
-                'content' => $html,
-                // any css to be embedded if required
-                'cssFile' => '@api/web/css/todolist.css',
-                // set mPDF properties on the fly
-                'options' => ['title' => "VISIT TO DO LIST"],
-                // call mPDF methods on the fly
-                'methods' => [
-                    'SetHeader' => [''],
-                    'SetFooter' => [''],
-                ],
-            ]);
-            $pdf->content = $html;
-            $file_name = "note_" . rand(7, 100) . "_" . time() . ".pdf";
-            $pdf->filename = "../../uploads/pdf_todolist/" . $file_name;
-            echo $pdf->render();
-            $emailformatemodel = EmailFormat::findOne(["title" => 'todolist_email', "status" => '1']);
-            if ($emailformatemodel) {
+                // setup kartik\mpdf\Pdf component
+                $pdf = new Pdf([
+                    // set to use core fonts only
+                    'mode' => Pdf::MODE_CORE,
+                    // A4 paper format
+                    'format' => Pdf::FORMAT_A4,
+                    // portrait orientation
+                    'orientation' => Pdf::ORIENT_PORTRAIT,
+                    // stream to browser inline
+                    'destination' => Pdf::DEST_FILE,
+                    // your html content input
+                    'content' => $html,
+                    // any css to be embedded if required
+                    'cssFile' => '@api/web/css/todolist.css',
+                    // set mPDF properties on the fly
+                    'options' => ['title' => "VISIT TO DO LIST"],
+                    // call mPDF methods on the fly
+                    'methods' => [
+                        'SetHeader' => [''],
+                        'SetFooter' => [''],
+                    ],
+                ]);
+                $pdf->content = $html;
+                $file_name = "note_" . rand(7, 100) . "_" . time() . ".pdf";
+                $pdf->filename = "../../uploads/pdf_todolist/" . $file_name;
+                echo $pdf->render();
+                $emailformatemodel = EmailFormat::findOne(["title" => 'todolist_email', "status" => '1']);
+                if ($emailformatemodel) {
 
-                $body = $emailformatemodel->body;
-                $ssSubject = $emailformatemodel->subject;
-                //send email for new generated password
-                $attach = !empty($file_name) && file_exists(Yii::getAlias('@root') . '/' . "uploads/pdf_todolist/" . $file_name) ? Yii::$app->params['root_url'] . '/' . "uploads/pdf_todolist/" . $file_name : "";
-                $ssResponse = Common::sendMailToUserWithAttachment($requestParam['to_patient_email'], $fromEmail, $ssSubject, $body, $attach);
-                if ($ssResponse) {
-                    $toDoListModel = new TodoList();
-                    $toDoListModel->user_id = $requestParam['user_id'];
-                    $toDoListModel->investigator = $requestParam['investigator'];
-                    $toDoListModel->protocol = $requestParam['protocol'];
-                    $toDoListModel->date = $requestParam['date'];
-                    $toDoListModel->list = $requestParam['list'];
-                    $toDoListModel->patient_id = !empty($requestParam['patient_id']) ? $requestParam['patient_id'] : "";
-                    $toDoListModel->to_patient_email = $requestParam['to_patient_email'];
-                    $toDoListModel->pdf_file_name = Yii::$app->params['root_url'] . "/uploads/pdf_todolist/" . $file_name;
-                    $toDoListModel->save(false);
-                    $toDoList[] = $toDoListModel;
+                    $body = $emailformatemodel->body;
+                    $ssSubject = $emailformatemodel->subject;
+                    //send email for new generated password
+                    $attach = !empty($file_name) && file_exists(Yii::getAlias('@root') . '/' . "uploads/pdf_todolist/" . $file_name) ? Yii::$app->params['root_url'] . '/' . "uploads/pdf_todolist/" . $file_name : "";
+                    $ssResponse = Common::sendMailToUserWithAttachment($requestParam['to_patient_email'], $fromEmail, $ssSubject, $body, $attach);
+                    if ($ssResponse) {
+                        $toDoListModel = new TodoList();
+                        $toDoListModel->user_id = $requestParam['user_id'];
+                        $toDoListModel->investigator = $requestParam['investigator'];
+                        $toDoListModel->protocol = $requestParam['protocol'];
+                        $toDoListModel->date = $requestParam['date'];
+                        $toDoListModel->list = $requestParam['list'];
+                        $toDoListModel->patient_id = !empty($requestParam['patient_id']) ? $requestParam['patient_id'] : "";
+                        $toDoListModel->to_patient_email = $requestParam['to_patient_email'];
+                        $toDoListModel->pdf_file_name = Yii::$app->params['root_url'] . "/uploads/pdf_todolist/" . $file_name;
+                        $toDoListModel->save(false);
+                        $toDoList[] = $toDoListModel;
+                    }
+
                 }
 
+                $amReponseParam = $toDoList;
+                $ssMessage = 'To do list PDF is successfully sent through email.';
+                $amResponse = Common::successResponse($ssMessage, $amReponseParam);
+            } else {
+                $ssMessage = 'Invalid user_id';
+                $amResponse = Common::errorResponse($ssMessage);
             }
-
-            $amReponseParam = $toDoList;
-            $ssMessage = 'To do list PDF is successfully sent through email.';
-            $amResponse = Common::successResponse($ssMessage, $amReponseParam);
         } else {
-            $ssMessage = 'Invalid user_id';
+            $ssMessage = 'List can not be blank';
             $amResponse = Common::errorResponse($ssMessage);
         }
         Common::encodeResponseJSON($amResponse);
+
     }
 
     public function actionSendPdfActionItems()
