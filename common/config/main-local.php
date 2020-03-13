808c558429b6fcff110e14c362e78e76da741366
root <root@localhost.creatusello.com>
2020-03-13 13:39:47 +0530

    add common config mail

--- common/config/main-local.php
+++ common/config/main-local.php
@@ -14,7 +14,7 @@ return [
 // send all mails to a file by default. You have to set
             // 'useFileTransport' to false and configure a transport
             // for the mailer to send real emails.
-            'useFileTransport' => true,
+            'useFileTransport' => false,
 //'useFileTransport' => false,//to send mails to real email addresses else will get stored in your mail/runtime folder
             //comment the following array to send mail using php's mail function
             /*          'transport' => [
@@ -32,21 +32,21 @@ return [
             ],
             ],
             ],*/
-            'transport' => [
-                'class' => 'Swift_SmtpTransport',
-                'host' => 'mail.clinessentials.com',
-                'username' => 'h322zkksbfpr',
-                'password' => '?x&W:;E&1m5',
-                'port' => '587',
-                'encryption' => 'tls',
-                'streamOptions' => [
-                    'ssl' => [
-                        'allow_self_signed' => true,
-                        'verify_peer' => false,
-                        'verify_peer_name' => false,
-                    ],
-                ],
-            ],
+            /*         'transport' => [
+        'class' => 'Swift_SmtpTransport',
+        'host' => 'mail.clinessentials.com',
+        'username' => 'h322zkksbfpr',
+        'password' => '?x&W:;E&1m5',
+        'port' => '587',
+        'encryption' => 'tls',
+        'streamOptions' => [
+        'ssl' => [
+        'allow_self_signed' => true,
+        'verify_peer' => false,
+        'verify_peer_name' => false,
+        ],
+        ],
+        ],*/
         ],
     ],
 ];
